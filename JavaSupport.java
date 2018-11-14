33893df1c37089f24483960952851967e58348c9
yangbinzuishuai <44902593+yangbinzuishuai@users.noreply.github.com>
2018-11-14 13:42:13 +0800

    Update JavaSupport.java
    确定程序运行时间，模板设计模式，多态，抽象类

--- JavaSupport.java
+++ JavaSupport.java
@@ -56,3 +56,31 @@ public class JavaSupport {
     }
 }
 
+
+//确定程序运行时间，模板设计模式，多态，抽象类
+abstract class Demo {
+    public final void getTime() {
+        long start = System.currentTimeMillis();
+        function();
+        long stop = System.currentTimeMillis();
+        System.out.println();
+        System.out.println("共花费" + (stop - start));
+    }
+
+    public abstract void function();
+}
+class Test extends Demo {//重写，可以根据自己的需要更改function方法里面的内容
+
+    public void function() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.print("aa");
+        }
+    }
+}
+public class TimeMillis {
+    public static void main(String[] args) {
+        Test t=new Test();
+        t.getTime();
+    }
+}
+
